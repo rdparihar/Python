b1cc9ecc51c1e9a67e0216df410b1e33c5874bfa
Rajdeep Singh Parihar <rdparihar@yahoo.com>
2017-09-15 20:31:52 +0530

    Uploading of code
    from local machine

---
+++ cough2.py
@@ -0,0 +1,8 @@
+def main():
+    for i in range (3):
+        cough()
+
+def cough():
+    print("cough")
+if __name__ == '__main__':
+    main()
---
+++ loop_while.py
@@ -0,0 +1,9 @@
+s = input("name: ")
+
+count = 0
+while count < 9:
+    print('{}'.format(s))
+    count = count + 1
+print ("Good Bye {}" .format(s))
+
+# this will print the Ankit to 9 times.
